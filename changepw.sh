8b5f195f93e93faa18b58c142c9b55eb65158855
dungbkhn <86149640+dungbkhn@users.noreply.github.com>
2022-12-11 22:22:31 +0700

    Update changepw.sh

--- changepw.sh
+++ changepw.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 
-op="$1"
-np1="$2"
-np2="$2"
+#op="$1"
+#np1="$2"
+#np2="$2"
 
-echo -e "$op""\n""$np1""\n""$np2" | passwd
+#echo -e "$op""\n""$np1""\n""$np2" | passwd
+
+
+np1="$1"
+np2="$1"
+
+echo -e "$np1""\n""$np2" | passwd store
 
